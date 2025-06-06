f0b23c1e7b7deeb72998bdf56c9c1e9f3f716037
BihaBen <13.bence.b@gmail.com>
2025-06-06 16:02:51 +0200

    Add Players.sql

---
+++ schema/Players.sql
@@ -0,0 +1,15 @@
+-- Players and their details.
+CREATE TABLE Players (
+	
+	-- Identification number for player.
+    PlayerID INT IDENTITY(1,1) PRIMARY KEY,
+	
+	-- First name.
+    PlayerFirstName NVARCHAR(50) NOT NULL,
+	-- Last name.
+    PlayerSecondName NVARCHAR(50) NOT NULL,
+	-- Birthsday date.
+    DateOfBirth DATE NOT NULL,
+	-- Birth place (country).
+    CountryOfBirth NVARCHAR(50),
+);
